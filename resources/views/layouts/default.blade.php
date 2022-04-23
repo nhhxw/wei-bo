475e0215ea4279e5a86ab85d4217d19145172725
hxw <1040122957@qq.com>
2022-04-23 15:11:39 +0800

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,12 @@
 <html>
 <head>
     <title>@yield('title', 'Weibo App')- Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
+
+@include('layouts._header')
 @yield('content')
+@include('layouts._footer')
 </body>
 </html>
