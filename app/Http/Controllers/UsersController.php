2674f8f9e801b78103acadd654673fcb3f134bec
hxw <1040122957@qq.com>
2022-05-01 23:44:35 +0800

    用户注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -10,4 +11,27 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+    public function show(Request $request, User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $data =  $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
+
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,14 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-
 @include('layouts._header')
-@yield('content')
-@include('layouts._footer')
+<div class="container">
+    <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
+        @include('shared._errors')
+        @yield('content')
+        @include('layouts._footer')
+    </div>
+</div>
 </body>
 </html>
